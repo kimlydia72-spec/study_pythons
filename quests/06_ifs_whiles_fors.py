28ea533db41c8345b10a1db03183926e448fdb53
Lydia kim <kimlydia72@gmail.com>
2025-11-18 16:35:36 +0900

    modify

--- quests/06_ifs_whiles_fors.py
+++ quests/06_ifs_whiles_fors.py
@@ -1,17 +1,17 @@
 # 문제 1 — 문자열 포맷 오류 수정하기
 
-# second = "Programming"
-# first = "Welcome to Python Strings" + second   # 오류 발생
+second = "Programming"
+first = "Welcome to Python Strings" + second   # 오류 발생
 
-# print(first)
+print(first)
 
 # 문제 2 — while 조건 오류 찾기
 
-# first = "Hello Python"
+first = "Hello Python"
 
-# while len(first) > 0:
-#     print(first)
-#     first = first[:-1]
+while len(first) > 0:
+    print(first)
+    first = first[:-1]
 
 #   오류 설명 : 
 #   TypeError가 발생합니다. **문자열("Hello Python")**과 정수(0 또는 1) 간의 
@@ -24,19 +24,19 @@
                                                                     
 # 문제 3 — 리스트 누적 합 오류 찾기
 
-# kor = [70, 80, 90, 40, 50]
-# eng = [90, 80, 70, 70, 60]
-# sum_all = 0
+kor = [70, 80, 90, 40, 50]
+eng = [90, 80, 70, 70, 60]
+sum_all = 0
 
-# sum_all = sum(kor) + sum(eng)
-# print(f'전체 합계 : {sum_all}')
+sum_all = sum(kor) + sum(eng)
+print(f'전체 합계 : {sum_all}')
 
 # 문제 4 — for-range 인덱스 문제 해결
 
-# kor = [70, 80, 90, 40, 50]
-# eng = [90, 80, 70, 70, 60]
+kor = [70, 80, 90, 40, 50]
+eng = [90, 80, 70, 70, 60]
 
-# sum_total = 0
+sum_total = 0
 
-# for i in range(0, 10):    # 오류 발생
-#     sum_total = sum_total + kor[i] + eng[i]
+for i in range(0, 10):    # 오류 발생
+    sum_total = sum_total + kor[i] + eng[i]
